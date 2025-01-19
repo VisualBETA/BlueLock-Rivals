305907a2437faee0c3eba776fb80500994c95415
tevfn <awdawdawdawdaw85@gmail.com>
2025-01-19 05:50:45 +0400

    CodeLoader.lua

--- CodeLoader.lua
+++ CodeLoader.lua
@@ -1,5 +1,6 @@
 --// [🏂]
 
-return {
-    {'25KTATLIS', true}, {'300KREO', true}, {'35KCHROLLO', true}, {'THANKYOU', true}, {'20KLIKESPT2', true}
+return {YUKIMIYA
+    {'25KTATLIS', true}, {'300KREO', true}, {'35KCHROLLO', true}, {'THANKYOU', true}, {'20KLIKESPT2', true},
+    {'YUKIMIYA', true}
 }
