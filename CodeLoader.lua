ed19780754e1de3a20daa5a9fb056acbd93134c4
tevfn <awdawdawdawdaw85@gmail.com>
2025-01-21 21:34:39 +0400

    CodeLoader.lua

--- CodeLoader.lua
+++ CodeLoader.lua
@@ -5,6 +5,8 @@ return {
     {'300KREO', false},
     {'35KCHROLLO', false},
     {'THANKYOU', false},
-    {'20KLIKESPT2', true},
-    {'YUKIMIYA', true}
+    {'20KLIKESPT2', false},
+    {'YUKIMIYA', false},
+    {'60KFOLLOWERS', true},
+    {'30KLIKES', true}
 }
